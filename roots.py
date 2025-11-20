6dd5c4129e744a4f140c3040fec45269b2f90a20
Katia <katia.punter@storyshareplatform.com>
2025-11-20 22:08:25 +0300

    fixes a bug

--- roots.py
+++ roots.py
@@ -2,15 +2,6 @@ def sq_num(n):
     """Return the square root of a given number."""
     return n**0.5
 
-while True:
+if __name__ == "__main__":
     user_input = input("Enter a positive number: ")
-    try:
-        number = float(user_input)  # ensure the input can be turned into a number
-        if number >= 0:
-            break
-        else:
-            print("Please enter zero or more.")
-    except ValueError:
-        print("Please enter a valid number.")
-
-print("The square root is:", sq_num(number))
+    print("The square root is:", sq_num(user_input))
